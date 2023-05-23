c5f4dd6598ec29030f73b909735decc9c02fb8a9
murakami <nori@vbminigame.sakura.ne.jp>
2023-05-23 15:21:07 +0900

    mapping修正

--- src/main/java/com/bookmanager/book/controller/BookController.java
+++ src/main/java/com/bookmanager/book/controller/BookController.java
@@ -8,17 +8,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bookmanager.book.entity.Book;
 import com.bookmanager.book.service.BookService;
 
 @Controller
+@RequestMapping("/book")
 public class BookController {
 
     @Autowired
     BookService BookService;
     
-    @GetMapping("/list")
+    @GetMapping
     private String list(@ModelAttribute User user, Model model) {
         List<Book> bookList = BookService.search();
         model.addAttribute("bookList", bookList);
