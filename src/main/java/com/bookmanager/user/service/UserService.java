d3bae4897d3ed15ade13baaa677205aa21705ad6
nori <nori@vbminigame.sakura.ne.jp>
2023-06-18 19:11:22 +0900

    ユーザ追加実装中

--- src/main/java/com/bookmanager/user/service/UserService.java
+++ src/main/java/com/bookmanager/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.bookmanager.user.service;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.bookmanager.book.repository.UserMapper;
 
@@ -11,11 +12,16 @@ public class UserService {
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+	BCryptPasswordEncoder passwordEncoder;
+
     public boolean existUserId(Integer userId) {
         return Objects.nonNull(userMapper.findUserId(userId));
     }
 
     public void add(Integer userId, String userName, String password, String passwordCheck) {
+        String encodePassword=passwordEncoder.encode(password);
+        userMapper.save(userId, userName, encodePassword);
     }
 
 }
